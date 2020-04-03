23a139ea60186238702062ba26eebbce5583f527
Bvin <bvin@live.cn>
2020-04-03 16:55:30 +0800

    发现Tab

--- lib/tabs/explore_tab.dart
+++ lib/tabs/explore_tab.dart
@@ -1,5 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:html/dom.dart' as html;
+import '../gallary_page.dart';
+import 'package:dio/dio.dart';
+import 'dart:convert';
 
 class ExploreTab extends StatefulWidget{
 
@@ -15,8 +20,16 @@ class ExploreTab extends StatefulWidget{
 
 class TabState extends State<ExploreTab>{
 
+  Dio _dio;
+  bool _showLoading = false;
+  List<html.Element> _photos;
+  int _lastId = 1;
+
   @override
   void initState() {
+    _dio = Dio();
+    _photos = List();
+    load(_lastId);
     super.initState();
   }
 
@@ -40,8 +53,17 @@ class TabState extends State<ExploreTab>{
             ),
           )
         ],
-        body: Container(),
+        body: gridBody(),
     );
+  }
+
+  gridBody(){
+    List<Widget> children = [];
+    if(_photos != null){
+      children.add(grid());
+    }
+    children.add(loading());
+    return Stack(children: children,);
   }
 
   title() {
@@ -101,6 +123,61 @@ class TabState extends State<ExploreTab>{
       )).toList(),
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
     );
+  }
+
+  grid(){
+    return StaggeredGridView.countBuilder(
+      mainAxisSpacing: 2,
+      crossAxisSpacing: 2,
+      crossAxisCount: 2,
+      itemCount: _photos.length,
+      itemBuilder: (ctx,index)=> GestureDetector(
+        child: CachedNetworkImage(imageUrl: _photos[index].attributes["src"]),
+        onTap: (){
+          Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => GalleryPage(_photos, index)));
+        },
+      ),
+      staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+    );
+  }
+
+  loading(){
+    return Center(
+      child: Visibility(
+        child: CircularProgressIndicator(),
+        visible: _showLoading,
+      ),
+    );
+  }
+
+  load(lastId) async {
+    Response response = await _dio.get("https://m1.35photo.pro/show_block.php",
+        options: Options(headers: {
+          "Cookie":"user_lang=en; _ga=GA1.2.1675562080.1585727189; _gid=GA1.2.1999997836.1585727189; _fbp=fb.1.1585727189511.1481881805; _ym_uid=15857915721044290429; _ym_d=1585791572; _ym_isad=2; user_login=bvin; token2=300d307489ac74db963ce362ae43833d; user_status=new; nude=true; me=fe9c78a3ad3f2178a32a04050d5de96d; session=6kcskbrq7ngidcgkhr60l9pkuh; user_lastEnter=1585818915 If-Modified-Since: Thu, 02 Apr 2020 09:14:48 GMT",
+        }),
+        queryParameters: {
+          "type": "getNextPageData",
+          "page": "new",
+          "lastId": lastId,
+          "prevNew": true,
+          "part": "actual",
+        }
+    );
+    String data = response.data;
+    Map map = json.decode(data);
+    //_lastId = map["lastId"];
+    html.Document document = html.Document.html(map["data"]);
+    List<html.Element> elements = document.getElementsByClassName("showPrevPhoto");
+    _photos.addAll(elements);
+    setState(() {});
+    elements.forEach((e){
+      String src = e.attributes["src"];
+      String alt = e.attributes["alt"];
+      String title = e.attributes["title"];
+      print(e);
+    });
+
+    //print(map["data"]);
   }
 }
 
