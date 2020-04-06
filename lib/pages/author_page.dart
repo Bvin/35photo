e6abc3703947b5d4f1687a9bd0b890301424ba29
Bvin <bvin@live.cn>
2020-04-06 15:27:26 +0800

    个人页面作品列表加载

--- lib/pages/author_page.dart
+++ lib/pages/author_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
@@ -23,8 +25,9 @@ class PageState extends  State<AuthorPage>{
   Dio _dio;
   bool _showLoading = false;
   List<html.Element> _photos;
-  int _lastId = 1;
+  String _lastId = "";
   Map _profile = Map();
+  String _user_id = "";
 
   @override
   void initState() {
@@ -40,12 +43,16 @@ class PageState extends  State<AuthorPage>{
       home: Scaffold(
         body: SafeArea(child: body()),
       ),
+      theme: ThemeData.dark(),
     );
   }
 
   load() async {
-    Response response = await _dio.get(widget.url);
-    html.Document document = html.Document.html(response.data);
+    Response response = await _dio.get(widget.url, options: Options(
+      headers: {"Cookie":"user_login=bvin;token2=300d307489ac74db963ce362ae43833d;nude=true;"}
+    ));
+    String data = response.data;
+    html.Document document = html.Document.html(data);
     html.Element profileContainer = document.getElementsByClassName("container-fluid shadowFont")[0];
     String style = profileContainer.attributes["style"];
     String background = style.substring(style.indexOf('('),style.lastIndexOf(')'));
@@ -65,6 +72,17 @@ class PageState extends  State<AuthorPage>{
     _profile["photocount"] = counters[1].children[0].children[0].text;
     _profile["view"] = counters[2].children[0].children[0].text;
     setState(() {});
+
+    String param = data.substring(
+        data.indexOf("user_id="), data.lastIndexOf("cantSetLike "));
+    String user_id = param.substring("user_id=".length, param.indexOf('";'));
+    _user_id = user_id;
+    String last_id = param.substring(
+        param.indexOf("showNextListId") + "showNextListId".length + 1,
+        param.lastIndexOf(';'));
+    _lastId = last_id;
+    loadPhotos();
+    print(last_id);
   }
 
   body() {
@@ -157,4 +175,33 @@ class PageState extends  State<AuthorPage>{
       ),
     );
   }
+
+  loadPhotos() async {
+      Response response = await _dio.get("https://m1.35photo.pro/show_block.php",
+          options: Options(headers: {
+            "Cookie":"user_lang=en; _ga=GA1.2.1675562080.1585727189; _gid=GA1.2.1999997836.1585727189; _fbp=fb.1.1585727189511.1481881805; _ym_uid=15857915721044290429; _ym_d=1585791572; _ym_isad=2; user_login=bvin; token2=300d307489ac74db963ce362ae43833d; user_status=new; nude=true; me=fe9c78a3ad3f2178a32a04050d5de96d; session=6kcskbrq7ngidcgkhr60l9pkuh; user_lastEnter=1585818915 If-Modified-Since: Thu, 02 Apr 2020 09:14:48 GMT",
+          }),
+          queryParameters: {
+            "type": "getNextPageData",
+            "page": "photoUser",
+            "lastId": _lastId,
+            "user_id": _user_id,
+          }
+      );
+      String data = response.data;
+      Map map = json.decode(data);
+      //_lastId = map["lastId"];
+      html.Document document = html.Document.html(map["data"]);
+      List<html.Element> elements = document.getElementsByClassName("showPrevPhoto");
+      _photos.addAll(elements);
+      setState(() {});
+      elements.forEach((e){
+        String src = e.attributes["src"];
+        String alt = e.attributes["alt"];
+        String title = e.attributes["title"];
+        print(e);
+      });
+
+      //print(map["data"]);
+    }
 }
